9c36aa09b1d27cf4b6c97ea26ae14a9ae81ca4aa
Guest User <Guest@iMac-9.local>
2024-03-12 20:39:29 +0100

    Small UI Changes

--- AKTest-iOS/ViewController.swift
+++ AKTest-iOS/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstLabel: UILabel!
+    @IBOutlet weak var SecondLabel: UILabel!
+    @IBOutlet weak var ThirdLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        FirstLabel.text = "FirstLabel"
+        SecondLabel.text = "SecondLabel"
+        ThirdLabel.text = "sdhhbgzeliyghbeubnuehgzeuffhuiryhghjtyjhrhrhTEHHETHRTHrthtyjhtyJUYJKUYèt§yzèeifyoèZEFZET3454ZET4634542653"
     }
 
 
